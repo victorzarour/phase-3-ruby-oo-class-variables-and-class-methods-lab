bdbb28b3158827e2432e761749b4558c42540aa6
Victor <vx_zz@hotmail.com>
2022-06-29 08:47:36 -0400

    commit tree ef6a412ff3205dbbba60dade4748928ab60fae77

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Song
+
+    attr_accessor :name, :artist, :genre
+
+    def initialize (name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre 
+
+        @@count += 1
+        @@genres << self.genre
+        @@artists << self.artist
+    end
+
+    @@count = 0
+    @@genres = []
+    @@artists = []
+
+    def self.count
+        @@count
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genre_count
+        @@genres.tally
+    end
+
+    def self.artist_count
+        @@artists.tally
+    end
+
+end
